459a2229385000cf9b57d69a803d5eb8359b9f22
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2018-08-06 20:55:09 +0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("hello-world");
     }
 }
